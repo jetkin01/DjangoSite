1e124f9a76fd3eef6ad9ce8eb8ae9dd06c59f7c1
Josh Etkind <etkind.j@gmail.com>
2020-08-23 11:44:48 -0400

    Finished part 3 of tutorial

--- mysite/README.md
+++ mysite/README.md
@@ -1,12 +1,10 @@
 # mysite is my django-powered website!
 
 ## About
-I've been following this tutorial[link:https://docs.djangoproject.com/en/3.1/intro/tutorial01/]
+I've been following this [tutorial](link:https://docs.djangoproject.com/en/3.1/intro/tutorial01/)
 - Completed part 1
 - Completed part 2
-- About halfway through part 3, at the 'Raising a 404 error' section
-
----
+- Completed part 3
 
 ## To Run
 
@@ -20,8 +18,6 @@ I've been following this tutorial[link:https://docs.djangoproject.com/en/3.1/int
 - navigate to `http://127.0.0.1:8000` in a browser
 	- /polls displays index page
 	- /polls/# displays question number #
-
----
 
 ## Thoughts
 - migrations seem to be the solution to the DAR we (Meridian) had friday 8/21 no???
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,13 +4,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
-	# ex: /polls/
-	path('', views.index, name='index'),
-	# ex: /polls/6/
-	path('<int:question_id>/', views.detail, name='detail'),
-	# ex: /polls/5/results/
-	path('<int:question_id>/results/', views.results, name='results'),
-	#ex: /poll/5/vote/
-	path('<int:question_id>/vote/', views.vote, name='vote'),
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/6/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /poll/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
